37aca4361ab7b91f6ef040160de96308dc2cff55
AntonioUCode <antoniouc38@outlook.com>
2021-07-24 17:33:30 -0500

    primer commit alv

---
+++ app/Controllers/Add_datos.php
@@ -0,0 +1,13 @@
+<?php 
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Add_datos extends Controller{
+
+    public function index()
+    {
+        return view('registro_datos');
+    }
+
+}
